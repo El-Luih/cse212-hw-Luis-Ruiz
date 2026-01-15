6a849bfef87e702557f8767530b8e481c4019338
El-Luih <luisalejandro632@gmail.com>
2026-01-15 15:12:12 -0600

    Week 1 Code

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -13,7 +13,27 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        /*
+        1. Create an empty fixed-size double array named results using length as its size. 
+            This array will store the multiples of the given number.
+        2. Use a for loop to multiply number by coefficients ranging from 1 to length. 
+            The loop should start with i = 0, continue while i < length, and increase i by 1 on each iteration.
+        3. Inside the loop, create a variable p (for product) that stores the result of
+            number * (i + 1), since the index starts at 0.
+        4. Still inside the loop, assign the value of p to the correct index of the results array using i.
+        5. After the loop finishes, return the results array.
+        */
+
+        var results = new double[length];
+
+        for (int i = 0; i < length; i++)
+        {
+            double p = number * (i + 1);
+            results[i] = p;
+        }
+
+
+        return results; // replace this return statement with your own
     }
 
     /// <summary>
@@ -29,5 +49,15 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        /*
+        1. Create a new List<int> element called "displaced" from a range of items of "data". This range starts at index 0 
+            and retrieves a number of items equal to the total amount of items minus the specified "amount" argument.
+        2. Remove the same range of items from the "data" list after retrieving their values.
+        3. Append the "displaced" array to the end of the modified "data" list.
+        */
+        List<int> displaced = data.GetRange(0, data.Count - amount);
+        data.RemoveRange(0, data.Count - amount);
+        data.AddRange(displaced);
     }
 }
